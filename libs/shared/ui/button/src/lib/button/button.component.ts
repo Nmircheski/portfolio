71658aa519051251263ed8e69c8454317714d688
Nikola Mircheski <nmircheski@gmail.com>
2025-05-13 14:52:19 +0200

    Introduce Card & Button components

---
+++ libs/shared/ui/button/src/lib/button/button.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {}
