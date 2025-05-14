659aa031bbc01d862022c1dab47a9403d6159f8a
Nikola Mircheski <nmircheski@gmail.com>
2025-05-14 16:34:59 +0200

    Setup components in app html

--- apps/my-portfolio/src/app/app.component.ts
+++ apps/my-portfolio/src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AboutMeComponent } from '@portfolio/about-me';
+import { ContactComponent } from '@portfolio/contact';
+import { ExperienceComponent } from '@portfolio/experience';
+import { HeroComponent } from '@portfolio/hero';
+import { SkillsComponent } from '@portfolio/skills';
+import { FooterComponent } from '@portfolio/ui/footer';
 import { NavbarComponent } from '@portfolio/ui/navbar';
+import { WorkComponent } from '@portfolio/work';
 
 @Component({
-  imports: [RouterModule, NavbarComponent],
+  imports: [
+    RouterModule,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    WorkComponent,
+    ContactComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
