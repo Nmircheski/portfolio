45863d76699617e8503f3f93adfd7cfe8da6b0b6
Nikola Mircheski <nmircheski@gmail.com>
2025-05-17 19:12:01 +0200

    Introduce about me section on mobile

--- libs/feature/about-me/src/lib/about-me/about-me.component.ts
+++ libs/feature/about-me/src/lib/about-me/about-me.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChipComponent } from '@portfolio/ui/chip';
 @Component({
   selector: 'portfolio-about-me',
-  imports: [CommonModule],
+  imports: [CommonModule, ChipComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
