239cffa47702fe056bd9caf7afd748920ceaac39
Nikola Mircheski <nmircheski@gmail.com>
2025-05-18 16:39:51 +0200

    Add footer - mobile

--- libs/shared/ui/footer/src/lib/footer/footer.component.ts
+++ libs/shared/ui/footer/src/lib/footer/footer.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IconComponent } from '@portfolio/ui/icon';
 @Component({
   selector: 'portfolio-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
