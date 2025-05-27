27e97f0b2c559f04b28cf4207ba93d13f6cb9013
Nikola Mircheski <nmircheski@gmail.com>
2025-05-27 19:45:36 +0200

    Wip

--- libs/feature/hero/src/lib/hero/hero.component.ts
+++ libs/feature/hero/src/lib/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IconComponent } from '@portfolio/ui/icon';
+import { PageSectionComponent } from '@portfolio/ui/page-section';
 import { HeroStatusInfo } from './model';
 @Component({
   selector: 'portfolio-hero',
-  imports: [CommonModule, IconComponent],
+  imports: [CommonModule, IconComponent, PageSectionComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
