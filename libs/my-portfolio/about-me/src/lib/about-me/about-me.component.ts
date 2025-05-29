f211cb97e084348af1c7fdb808adb5a9eff08cf7
Nikola Mircheski <nmircheski@gmail.com>
2025-05-29 20:41:56 +0200

    Introduce about me

--- libs/my-portfolio/about-me/src/lib/about-me/about-me.component.ts
+++ libs/my-portfolio/about-me/src/lib/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@portfolio/ui/button';
+import { IconComponent } from '@portfolio/ui/icon';
+import { PageSectionComponent } from '@portfolio/ui/page-section';
 
 @Component({
   selector: 'portfolio-about-me',
-  imports: [CommonModule],
+  imports: [CommonModule, PageSectionComponent, ButtonComponent, IconComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
