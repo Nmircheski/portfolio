3fd00144f9b6f82a8926009192ed47bc613616b3
Nikola Mircheski <nmircheski@gmail.com>
2025-05-29 21:24:43 +0200

    Introduce my-works

--- libs/my-portfolio/my-work/src/lib/my-work/my-work.component.ts
+++ libs/my-portfolio/my-work/src/lib/my-work/my-work.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PageSectionComponent } from '@portfolio/ui/page-section';
 
+import { CardComponent } from '@portfolio/ui/card';
 @Component({
   selector: 'portfolio-my-work',
-  imports: [CommonModule],
+  imports: [CommonModule, PageSectionComponent, CardComponent],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss',
 })
