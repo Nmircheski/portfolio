7f4ba55dbc7f304f33db8c43f4d33f72fed1e4d1
Nikola Mircheski <nmircheski@gmail.com>
2025-06-03 22:02:33 +0200

    Hero component - enhancemenets

--- libs/my-portfolio/hero/src/lib/hero/hero.component.ts
+++ libs/my-portfolio/hero/src/lib/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@portfolio/ui/button';
+import { CardComponent } from '@portfolio/ui/card';
 import { PageSectionComponent } from '@portfolio/ui/page-section';
 @Component({
   selector: 'portfolio-hero',
-  imports: [CommonModule, PageSectionComponent, ButtonComponent],
+  imports: [CommonModule, PageSectionComponent, ButtonComponent, CardComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
