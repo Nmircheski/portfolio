5fc359d9fcc11155b11f40ad4acc152b11930b71
Nikola Mircheski <nmircheski@gmail.com>
2025-06-03 14:53:59 +0200

    Fixes

--- libs/my-portfolio/hero/src/lib/hero/hero.component.ts
+++ libs/my-portfolio/hero/src/lib/hero/hero.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@portfolio/ui/button';
-import { IconComponent } from '@portfolio/ui/icon';
 import { PageSectionComponent } from '@portfolio/ui/page-section';
 @Component({
   selector: 'portfolio-hero',
-  imports: [CommonModule, PageSectionComponent, ButtonComponent, IconComponent],
+  imports: [CommonModule, PageSectionComponent, ButtonComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
