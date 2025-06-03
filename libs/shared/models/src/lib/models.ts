adad115d30e2a319f3db79827520bafb8c76c488
Nikola Mircheski <nmircheski@gmail.com>
2025-06-03 13:11:32 +0200

    Fonts & spacing fixes

--- libs/shared/models/src/lib/models.ts
+++ libs/shared/models/src/lib/models.ts
@@ -1,1 +1,10 @@
-export type Spacing = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+export type Spacing =
+  | 'none'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl';
