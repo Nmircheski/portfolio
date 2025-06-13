082dd2b47d76dcb439a34fecc925a932acc85e31
Nikola Mircheski <nmircheski@gmail.com>
2025-06-13 12:55:25 +0200

    Import components

--- libs/crypto/features/overview/shell/src/lib/crypto-overview-shell/crypto-overview-shell.component.ts
+++ libs/crypto/features/overview/shell/src/lib/crypto-overview-shell/crypto-overview-shell.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  BalanceCardComponent,
+  CurrencyCandlestickComponent,
+  CurrencyListComponent,
+  CurrencyTableComponent,
+  StatusWidgetComponent,
+} from '@portfolio/crypto-feature-overview-ui';
 @Component({
   selector: 'crypto-overview-crypto-overview-shell',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BalanceCardComponent,
+    CurrencyCandlestickComponent,
+    CurrencyListComponent,
+    CurrencyTableComponent,
+    StatusWidgetComponent,
+  ],
   templateUrl: './crypto-overview-shell.component.html',
   styleUrl: './crypto-overview-shell.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
