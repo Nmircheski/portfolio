ad085b0d13c3c3db3906b9729fea905c8f185b50
Nikola Mircheski <nmircheski@gmail.com>
2025-06-15 22:21:10 +0200

    Update currency list

--- libs/crypto/features/dashboard/ui/src/lib/components/currency-list/currency-list.component.ts
+++ libs/crypto/features/dashboard/ui/src/lib/components/currency-list/currency-list.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CardComponent } from '@portfolio/ui/card';
 import { CurrencyListItemComponent } from './list-item/list-item.component';
 
 @Component({
   selector: 'crypto-dashboard-currency-list',
-  imports: [CommonModule, CurrencyListItemComponent],
+  imports: [CommonModule, CurrencyListItemComponent, CardComponent],
   templateUrl: './currency-list.component.html',
   styleUrl: './currency-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
