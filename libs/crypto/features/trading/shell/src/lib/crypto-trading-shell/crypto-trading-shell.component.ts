b341b25a7b945047eb812a23b1354b933cbc7176
Nikola Mircheski <nmircheski@gmail.com>
2025-06-17 13:08:35 +0200

    setup layout for trading view

--- libs/crypto/features/trading/shell/src/lib/crypto-trading-shell/crypto-trading-shell.component.ts
+++ libs/crypto/features/trading/shell/src/lib/crypto-trading-shell/crypto-trading-shell.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TradingHeaderComponent } from '@portfolio/crypto-feature-trading-ui';
-
+import { CurrenciesListComponent } from '@portfolio/crypto-ui-currencies-list';
 @Component({
   selector: 'crypto-crypto-trading-shell',
-  imports: [CommonModule, TradingHeaderComponent],
+  imports: [CommonModule, TradingHeaderComponent, CurrenciesListComponent],
   templateUrl: './crypto-trading-shell.component.html',
   styleUrl: './crypto-trading-shell.component.scss',
 })
