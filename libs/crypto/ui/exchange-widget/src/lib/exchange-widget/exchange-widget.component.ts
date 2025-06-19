a7a29fe88ca95d07dafed605dd357c586f46e303
Nikola Mircheski <nmircheski@gmail.com>
2025-06-19 15:42:33 +0200

    Introduce initial template in Exchange Widget

--- libs/crypto/ui/exchange-widget/src/lib/exchange-widget/exchange-widget.component.ts
+++ libs/crypto/ui/exchange-widget/src/lib/exchange-widget/exchange-widget.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { ButtonComponent } from '@portfolio/ui/button';
 @Component({
   selector: 'crypto-ui-exchange-widget',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './exchange-widget.component.html',
   styleUrl: './exchange-widget.component.scss',
 })
