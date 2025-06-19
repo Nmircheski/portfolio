3cf3ff63ae4d6bf8a32d1d72e193c525cefe1b41
Nikola Mircheski <nmircheski@gmail.com>
2025-06-19 15:41:54 +0200

    Update ESlint

--- libs/crypto/features/trading/shell/eslint.config.mjs
+++ libs/crypto/features/trading/shell/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'crypto',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'crypto',
           style: 'kebab-case',
         },
       ],
