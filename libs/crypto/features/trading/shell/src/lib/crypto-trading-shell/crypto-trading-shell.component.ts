66744a3d9c9b0f57f23c9e037b6f10e8cef2898a
Nikola Mircheski <nmircheski@gmail.com>
2025-06-19 15:41:38 +0200

    Introduce exchange widget

--- libs/crypto/features/trading/shell/src/lib/crypto-trading-shell/crypto-trading-shell.component.ts
+++ libs/crypto/features/trading/shell/src/lib/crypto-trading-shell/crypto-trading-shell.component.ts
@@ -2,9 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TradingHeaderComponent } from '@portfolio/crypto-feature-trading-ui';
 import { CurrenciesListComponent } from '@portfolio/crypto-ui-currencies-list';
+import { ExchangeWidgetComponent } from '@portfolio/crypto-ui-exchange-widget';
 @Component({
   selector: 'crypto-crypto-trading-shell',
-  imports: [CommonModule, TradingHeaderComponent, CurrenciesListComponent],
+  imports: [
+    CommonModule,
+    TradingHeaderComponent,
+    CurrenciesListComponent,
+    ExchangeWidgetComponent,
+  ],
   templateUrl: './crypto-trading-shell.component.html',
   styleUrl: './crypto-trading-shell.component.scss',
 })
